13c55a039f71a90dc39b72e339917aae3ec5b75c
Ray Kim <raykim2526@gmail.com>
2022-06-08 22:24:17 -0400

    minor changes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-// const taskController = require("./controllers/task")
 require('./config/database');
 
 
